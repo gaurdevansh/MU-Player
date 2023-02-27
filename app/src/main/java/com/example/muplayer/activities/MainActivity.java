cabae7db2bb0d9fe59b38dd0d5f172fe9550168a
Devansh <devanshgaur75@gmail.com>
2023-02-28 01:34:12 +0530

    Added home, search and library fragment

--- app/src/main/java/com/example/muplayer/activities/MainActivity.java
+++ app/src/main/java/com/example/muplayer/activities/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.muplayer.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.example.muplayer.R;
+import com.example.muplayer.fragments.HomeFragment;
+import com.example.muplayer.fragments.LibraryFragment;
+import com.example.muplayer.fragments.SearchFragment;
 import com.example.muplayer.model.AudioItem;
 import com.example.muplayer.utils.AudioDataUtil;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
 
     private static final String TAG = "MainActivity";
     List<AudioItem> audioItemList = new ArrayList<>();
+    private FrameLayout fragContainer;
+    private BottomNavigationView bottomNavBar;
+    private HomeFragment homeFragment;
+    private LibraryFragment libraryFragment;
+    private SearchFragment searchFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +36,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         audioItemList = AudioDataUtil.getInstance().getAudioItemList();
+        Log.d(TAG, "onCreate: "+audioItemList.get(0).toString());
+        fragContainer = findViewById(R.id.fragContainer);
+        bottomNavBar = findViewById(R.id.bottomNavBar);
 
-        if(audioItemList!=null) {
-            for(AudioItem x: audioItemList) {
-                Log.d(TAG, "onCreate: "+x.toString());
-            }
+        homeFragment = new HomeFragment();
+        searchFragment = new SearchFragment();
+        libraryFragment = new LibraryFragment();
+
+        bottomNavBar.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+        bottomNavBar.setSelectedItemId(R.id.home);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragContainer, homeFragment).commit();
+                return true;
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragContainer, searchFragment).commit();
+                return true;
+            case R.id.library:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragContainer, libraryFragment).commit();
+                return true;
         }
+        return false;
     }
 }
